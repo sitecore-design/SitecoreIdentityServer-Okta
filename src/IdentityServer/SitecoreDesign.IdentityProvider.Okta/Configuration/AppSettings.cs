b02d0252c82f5c2a72892e9c8f5a85087ac3a606
@VincenzoLuigi <blah@blah.com>
2021-11-17 21:47:33 +1100

    Initial commit of the VS Solution only

---
+++ src/IdentityServer/SitecoreDesign.IdentityProvider.Okta/Configuration/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace SitecoreDesign.IdentityProvider.Okta.Configuration
+{
+    public class AppSettings
+    {
+        public static readonly string SectionName = "Sitecore:ExternalIdentityProviders:IdentityProviders:Okta";
+
+        public OktaIdentityProvider OktaIdentityProvider { get; set; } = new OktaIdentityProvider();
+    }
+}
---
+++ src/IdentityServer/SitecoreDesign.IdentityProvider.Okta/OktaIdentityProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace SitecoreDesign.IdentityProvider.Okta
+{
+    public class OktaIdentityProvider : Sitecore.Plugin.IdentityProviders.IdentityProvider
+    {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string Authority { get; set; }
+        
+        public string Scopes { get; set; }
+    }
+}
